486b6bcc1e0102f430f2a3ffa96777e0d765ed84
skoterev <skoterev@ford.com>
2025-04-28 17:25:01 -0400

    Implemented interfaces Device, Switchable, Dimmable, Networkable, TemperatureAdjustable, BeverageMaker. Made exising classes multiply inherited from these interfaces

--- smart_home.h
+++ smart_home.h
@@ -4,51 +4,87 @@
 #include <string>
 #include <vector>
 
-class SwitchableLight {
+class Device {
 public:
-    std::string turn_on() { return "Switchable light turned on"; }
-    std::string turn_off() { return "Switchable light turned off"; }
+    virtual ~Device() = default;
 };
 
-class DimmableLight {
+class Switchable {
 public:
-    std::string turn_on() { return "Dimmable light turned on at full brightness"; }
-    std::string turn_off() { return "Dimmable light turned off"; }
+    virtual ~Switchable()          = default;
+    virtual std::string turn_on()  = 0;
+    virtual std::string turn_off() = 0;
+};
 
-    std::string dim(int level_in_percent)
+class Dimmable {
+public:
+    virtual ~Dimmable()                           = default;
+    virtual std::string dim(int level_in_percent) = 0;
+};
+
+class Networkable {
+public:
+    virtual ~Networkable()                                                  = default;
+    virtual std::string connect_to_network(const std::string& network_name) = 0;
+};
+
+class TemperatureAdjustable {
+public:
+    virtual ~TemperatureAdjustable()                                = default;
+    virtual std::string set_temperature(int temperature_in_celcius) = 0;
+};
+
+class BeverageMaker {
+public:
+    virtual ~BeverageMaker()                                   = default;
+    virtual std::string brew(const std::string& beverage_type) = 0;
+};
+
+class SwitchableLight final : public Device, public Switchable {
+public:
+    std::string turn_on() override { return "Switchable light turned on"; }
+    std::string turn_off() override { return "Switchable light turned off"; }
+};
+
+class DimmableLight final : public Device, public Switchable, public Dimmable {
+public:
+    std::string turn_on() override { return "Dimmable light turned on at full brightness"; }
+    std::string turn_off() override { return "Dimmable light turned off"; }
+
+    std::string dim(int level_in_percent) override
     {
         return "Dimmable light dimmed to " + std::to_string(level_in_percent) + "%";
     }
 };
 
-class NetworkableLight {
+class NetworkableLight final : public Device, public Switchable, public Networkable {
 public:
-    std::string turn_on() { return "Networkable light turned on"; }
-    std::string turn_off() { return "Networkable light turned off"; }
+    std::string turn_on() override { return "Networkable light turned on"; }
+    std::string turn_off() override { return "Networkable light turned off"; }
 
-    std::string connect_to_network(const std::string& network_name)
+    std::string connect_to_network(const std::string& network_name) override
     {
         return "Networkable light connected to " + network_name;
     }
 };
 
-class AirConditioner {
+class AirConditioner final : public Device, public Switchable, public TemperatureAdjustable {
 public:
-    std::string turn_on() { return "AC turned on"; }
-    std::string turn_off() { return "AC turned off"; }
+    std::string turn_on() override { return "AC turned on"; }
+    std::string turn_off() override { return "AC turned off"; }
 
-    std::string set_temperature(int temperature_in_celcius)
+    std::string set_temperature(int temperature_in_celcius) override
     {
         return "Set temperature to " + std::to_string(temperature_in_celcius);
     }
 };
 
-class CoffeeMaker {
+class CoffeeMaker final : public Device, public Switchable, public BeverageMaker {
 public:
-    std::string turn_on() { return "Coffee maker turned on"; }
-    std::string turn_off() { return "Coffee maker turned off"; }
+    std::string turn_on() override { return "Coffee maker turned on"; }
+    std::string turn_off() override { return "Coffee maker turned off"; }
 
-    std::string brew(const std::string& coffee_type)
+    std::string brew(const std::string& coffee_type) override
     {
         return "Brewing " + coffee_type + " coffee";
     }
